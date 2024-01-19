87be95323f25220753cef51ed7ffa88455924cf3
pranavdaund <pranavdaund72@gmail.com>
2024-01-19 12:05:30 +0530

    Import the packages

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
 /**
  * Calculator
  */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color; 
+
 public class Calculator {
 
     
